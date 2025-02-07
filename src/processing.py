dcd18318dfb66b7c535381dbe4f33c845d1df957
Олег Усков <oleg81uskov@gmail.com>
2025-02-07 17:59:16 +0500

    init function ort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,3 @@
-
 from typing import Iterable
 
 
@@ -7,21 +6,22 @@ def filter_by_state(data_logs: Iterable[list[dict]], state: str = 'EXECUTED') ->
     соответствует указанному значению"""
     list_of_banking_transactions = []
     for item in data_logs:
-        for i in item:
-            if i['state'] == state:
-                list_of_banking_transactions.append(i)
+        for ds in item:
+            if ds['state'] == state:
+                list_of_banking_transactions.append(ds)
     return list_of_banking_transactions
 
 
-
-data_logs = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-filter_by_state(data_logs, state = 'EXECUTED')
-print(filter_by_state(data_logs, state = 'CANCELED'))
-
-
-def ort_by_date():
+def ort_by_date(data_logs: Iterable[list[dict]], reverse: bool = True) -> list[dict]:
     """Функция, которая возвращает список словарей, отсортированный по дате"""
-    pass
+    list_sorted_by_date = sorted(data_logs, key=lambda x: x['date'], reverse=reverse)
+    return list_sorted_by_date
+
+# data_logs = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+# filter_by_state(data_logs, state='EXECUTED')
+# ort_by_date(data_logs, reverse=True)
+# print(ort_by_date(data_logs, reverse=True))
+# print(filter_by_state(data_logs, state='EXECUTED'))
