34276ad7b45b63b72f9b2091563b17553b9ea94f
Олег Усков <oleg81uskov@gmail.com>
2025-02-17 16:09:31 +0500

    fixse test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -10,10 +10,24 @@ def test_get_mask_card_number():
 
 def test_get_mask_card_number_bug():
     with pytest.raises(ValueError) as exc_info:
-        get_mask_card_number(70007922896063612)
-        get_mask_card_number(700079228960636)
         get_mask_card_number("")
-        get_mask_card_number(2)
+    assert str(exc_info.value) == "Неверно введен номер банковской карты"
+
+
+def test_get_mask_card_number_bug1():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number(70007922896063612)
+    assert str(exc_info.value) == "Неверно введен номер банковской карты"
+
+
+def test_get_mask_card_number_bug2():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number(700079228960636)
+    assert str(exc_info.value) == "Неверно введен номер банковской карты"
+
+
+def test_get_mask_card_number_bug3():
+    with pytest.raises(ValueError) as exc_info:
         get_mask_card_number("ffffaaaalllooo")
     assert str(exc_info.value) == "Неверно введен номер банковской карты"
 
@@ -30,6 +44,16 @@ def test_get_mask_account(number, expected):
 def test_get_mask_account_empty_string():
     with pytest.raises(ValueError) as exc_info:
         get_mask_account("")
+    assert str(exc_info.value) == "Неверно введен номер банковского счета"
+
+
+def test_get_mask_account_string_bug():
+    with pytest.raises(ValueError) as exc_info:
         get_mask_account("qqqqqnnnnnzzzzzkkkkk")
+    assert str(exc_info.value) == "Неверно введен номер банковского счета"
+
+
+def test_get_mask_account_bug():
+    with pytest.raises(ValueError) as exc_info:
         get_mask_account(234)
     assert str(exc_info.value) == "Неверно введен номер банковского счета"
