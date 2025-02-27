06b32bae4115f9f98673e301bf59b6c03d7e70a5
Олег Усков <oleg81uskov@gmail.com>
2025-02-27 10:05:16 +0500

    fixse decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,12 +1,13 @@
 from typing import Any
+from functools import wraps
 
 
-def log(filename: str | bool = None) -> Any:
+def log(filename: Any = None) -> Any:
     """Декоратор, который автоматически логирует начало и конец выполнения функции, а
      также ее результаты или возникшие ошибки."""
-
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+    def decorator(func: Any) -> Any:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             print(f"Начало работы функции {func.__name__}")
             try:
                 result = func(*args, **kwargs)
@@ -24,11 +25,8 @@ def log(filename: str | bool = None) -> Any:
                     with open('mylog.txt', 'a', encoding='utf-8') as file:
                         file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs} \n")
                 print(f"Конец работы функции {func.__name__}")
-
         return wrapper
-
     return decorator
-
 
 # if __name__== "__main__":
 #
